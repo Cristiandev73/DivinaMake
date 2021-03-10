da7a3555c2dc101098dc6813ac1032aa761274e7
Cristian Bernardino <73257281+Cristiandev73@users.noreply.github.com>
2021-03-09 23:22:56 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+$('').slick({
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    speed: 600,
+    fade: true,
+    adaptiveHeight: true,
+    cssEase: 'linear'
+});$('.slider-principal').slick({
+    dots: false,
+    speed: 600,
+    autoplay: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    cssEase: 'linear'
+  });
